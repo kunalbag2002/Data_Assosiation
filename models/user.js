8b213fda209af3c9d42c8408b2d8943d63bf504f
kunalbag2002 <93586480+kunalbag2002@users.noreply.github.com>
2025-10-27 04:19:48 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 const mongoose= require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/mini-project')
+mongoose.connect('mongodb+srv://data_assosiation:Kunal2002.@cluster0.fudstpt.mongodb.net/mini-project')
+    .then(() => console.log('✅ Connected to MongoDB Atlas'))
+    .catch(err => console.log('❌ MongoDB Connection Error:', err));
 
 const userSchema= mongoose.Schema({
     username: String,
